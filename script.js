dc218fb0de23bebe41e536140d14bb4b180d973e
theapplesoup <65873849+theapplesoup@users.noreply.github.com>
2023-12-29 15:09:06 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function togglePlayer() {
     document.getElementById('player').classList.toggle('active');
 }
+
+function play() {
+
+    if (document.getElementById("media-btn").innerHTML.includes("play")) {
+        document.getElementById("media-btn").innerHTML = '<ion-icon name="pause">';
+    } else {
+        document.getElementById("media-btn").innerHTML = '<ion-icon name="play">';
+    }
+}
